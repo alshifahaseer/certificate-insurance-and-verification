b6be2019c12e9b9b847eb3d9cdaa40585f0dda41
alshifahaseer <alshifahaseer@gmail.com>
2025-08-13 12:41:48 +0530

    Update certificateinsuranceandverification.sol

--- contracts/certificateinsuranceandverification.sol
+++ contracts/certificateinsuranceandverification.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: mit
+// SPDX-License-Identifier: 
 pragma solidity ^0.8.19;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
@@ -197,3 +197,4 @@ contract Project is Ownable, ReentrancyGuard {
         insurancePool += msg.value;
     }
 }
+
